e1138f9c4cf16c282b3caefea224cea00534742c
glkt3912 <mole.gunma@gmail.com>
2023-10-04 00:02:40 +0900

    first commit(This Template Repositorys Clone)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
 
 /**
  * The root module of the application.
  */
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule { }
